c071a3444a08015a1d34cfd7e080f9587a0cae24
Kamil R <87485867+krybarski96@users.noreply.github.com>
2023-12-20 12:00:33 +0100

    second

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,15 +14,40 @@ export class AppComponent {
   constructor(private primengConfig: PrimeNGConfig) {
     this.primengConfig.ripple = true;
     
-    for (let i = 1; i <= 25; i++) {
-      const car: Car = {
-        name: "Samochód " + i,
-        tablica: "f3 lamel",
-        imgUrl: "https://static.autox.com/uploads/2023/07/Land-Rover-Range-Rover-Velar-Fuji-White.jpg",
-        isReserveted: true,
-        mileage: null
-      };
-      this.cars.push(car);
-    }
+    const car1: Car = {
+      name: "Range Rover Velar",
+      tablica: "F0 LAMEL",
+      imgUrl: "https://static.autox.com/uploads/2023/07/Land-Rover-Range-Rover-Velar-Fuji-White.jpg",
+      isReserveted: false,
+      mileage: null
+    };
+    
+    const car2: Car = {
+      name: "Skoda SUPERB",
+      tablica: "F3 LAMEL",
+      imgUrl: "https://www.bristolstreet.co.uk/images/new/vertu/car/skoda/superb/colour-swatches/metallic-moonwhite%5E950x634%5E.jpg",
+      isReserveted: false,
+      mileage: null
+    };
+    
+    const car3: Car = {
+      name: "Citroen Berlingo",
+      tablica: "F1 LAMEL",
+      imgUrl: "https://www.citroen.com.ge/wp-content/uploads/2015/07/gris_alu_nouveau_berlingo_vu_front.jpg",
+      isReserveted: false,
+      mileage: null
+    };
+    
+    const car4: Car = {
+      name: "Samochód 4",
+      tablica: "f3 lamel",
+      imgUrl: "https://media1.innovativcar-tech.de/12921-large_default/vw-lupo.jpg",
+      isReserveted: false,
+      mileage: null
+    };
+    
+    
+    // Dodawanie samochodów do tablicy cars
+    this.cars.push(car1, car2, car3, car4);
   }
 }
